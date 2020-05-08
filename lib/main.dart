907222934500ca95f0bbb59bdaadbf13a41df77b
Leonardo Ventocilla <ventocilla.leo@gmail.com>
2020-05-07 23:11:16 -0300

    Initial setup - pubspec updated

---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Pet UI',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.deepOrangeAccent,
+      )
+      ,
+      body: Container(),
+    );
+  }
+}
